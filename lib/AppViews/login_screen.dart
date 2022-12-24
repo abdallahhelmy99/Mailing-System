df78ee5d761cbf88792346f837d6dcada3298939
abdallahhelmy99 <abdallahhussam79@gmail.com>
2022-12-25 00:11:45 +0200

    Abdo's Latest

--- lib/AppViews/login_screen.dart
+++ lib/AppViews/login_screen.dart
@@ -96,7 +96,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             for (int i = 0; i < globalVariables.Users!.length; i++) {
                               if (mail.text == globalVariables.Users![i].email && pass.text == globalVariables.Users![i].password) {
                                 
-                                globalVariables.currentUser = new User(fname: globalVariables.Users?[i].fname, lname: globalVariables.Users?[i].lname, phoneNo: globalVariables.Users?[i].phoneNo, dob: globalVariables.Users?[i].dob, email: globalVariables.Users?[i].email, password:globalVariables.Users?[i].password
+                                globalVariables.currentUser =  User(fname: globalVariables.Users?[i].fname, lname: globalVariables.Users?[i].lname, phoneNo: globalVariables.Users?[i].phoneNo, dob: globalVariables.Users?[i].dob, email: globalVariables.Users?[i].email, password:globalVariables.Users?[i].password
                                 );        
                                 mail.clear();
                                 pass.clear();
