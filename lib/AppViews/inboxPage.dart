03f6be41dc698506ce01ab94616bb0ad8eecfc6c
hazzemh <hazemhossam12122001@gmail.com>
2022-12-28 20:33:26 +0200

    h

--- lib/AppViews/inboxPage.dart
+++ lib/AppViews/inboxPage.dart
@@ -225,7 +225,7 @@ class _inboxPageState extends State<inboxPage> {
                       width: width - 30,
                       textController: text,
                       onSuffixTap: () {},
-                      onSubmitted: (String) {},
+                       onSubmitted: (String ) {  },
                     ),
                   ),
                 ],
