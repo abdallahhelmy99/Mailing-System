12f2aa98520c5b1a9fbbd884a06bc0ea1aaa5dd3
basselamr <basselkamel20@gmail.com>
2022-12-29 02:40:51 +0200

    New dp

--- lib/dbHelper.dart
+++ lib/dbHelper.dart
@@ -26,7 +26,8 @@ class dbHelper {
         'CREATE TABLE "Users" (userID INTEGER PRIMARY KEY AUTOINCREMENT, fname TEXT NOT NULL, lname TEXT NOT NULL, dob DATETIME, email TEXT NOT NULL, password TEXT NOT NULL, phonenum TEXT NOT NULL, workExp TEXT, education TEXT)');
     await db.execute(
         'CREATE TABLE "Mail" (emailID INTEGER PRIMARY KEY AUTOINCREMENT, subject TEXT NOT NULL, body TEXT NOT NULL, trash Boolean, important Boolean , spam Boolean , isRead Boolean, draft Boolean, date DATETIME, senderID INTEGER NOT NULL, receiverID INTEGER NOT NULL, isSent Boolean)');
-    //await db.execute('CREATE TABLE "Contacts" (contact_ID INTEGER PRIMARY KEY AUTOINCREMENT,contactname TEXT NOT NULL , contactemail TEXT NOT NULL,  phonenum TEXT NOT NULL, relation TEXT NOT NULL )');
+    await db.execute(
+        'CREATE TABLE "Contacts" (userID int,contact_ID INTEGER ,contactname TEXT NOT NULL , contactemail TEXT NOT NULL,  phonenum TEXT NOT NULL, relation TEXT NOT NULL )');
     //await db.execute('CREATE TABLE "CONTACT_USER_RELATION" (userID INTEGER FOREIGN KEY AUTOINCREMENT, contact_ID INTEGER FOREIGN KEY AUTOINCREMENT,)');
     // await db.execute('CREATE TABLE "GROUP"(groupname TEXT NOT NULL, userID INTEGER AUTOINCREMENT, groupID INTEGER PRIMARY KEY AUTOINCREMENT)');
     // await db.execute('CREATE TABLE "GroupContent"(userID INTEGER AUTOINCREMENT, groupID INTEGER PRIMARY KEY AUTOINCREMENT, groupname TEXT NOT NULL, email TEXT NOT NULL, emailID INTEGER AUTOINCREMENT )');
